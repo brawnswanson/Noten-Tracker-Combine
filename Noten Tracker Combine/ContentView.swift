d6200755ea84231d5266106ae4226a651cf9f1dc
Daniel Pressner <daniel.pressner@gmail.com>
2021-11-21 13:35:47 +0100

    Initial Commit

---
+++ Noten Tracker Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Noten Tracker Combine
+//
+//  Created by Daniel Pressner on 21.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Noten Tracker Combine/Noten_Tracker_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noten_Tracker_CombineApp.swift
+//  Noten Tracker Combine
+//
+//  Created by Daniel Pressner on 21.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Noten_Tracker_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
